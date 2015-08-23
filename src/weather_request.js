eeb8629027b0b8010da832ba96a9fc702457e5cb
Matt McClellan <mcclellan.mj@gmail.com>
2015-08-23 10:38:53 -0500

    Images
    Added images to the repo

--- src/weather_request.js
+++ src/weather_request.js
@@ -10,7 +10,7 @@ function generateUrl(lat, lon) {
   return "http://api.wunderground.com/api/" + API_KEY + "/q/" + lat + "," + lon + ".json";
 }
 
-function locationSuccess(position) {
+function retrieveWeather(position) {
   console.log('lat= ' + position.coords.latitude + ' lon= ' + position.coords.longitude);
 }
 
@@ -21,8 +21,14 @@ function locationError(error) {
 Pebble.addEventListener('ready',
   function(e) {
     console.log('JavaScript app ready and running!');
-    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+    navigator.geolocation.getCurrentPosition(retrieveWeather, locationError, locationOptions);
     console.log('Generated url [' + generateUrl(37.776289, -122.395234) + ']');
+  }
+);
+
+Pebble.addEventListener('fetch_weather', 
+  function(e) {
+    console.log('Got request to fetch weather');
   }
 );
 // http://api.wunderground.com/api/{YOUR-API-KEY}/hourly/q/37.776289,-122.395234.json

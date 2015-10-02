efc83e36d01624ffb5aac9887fc76d230d5eb390
Matt McClellan <mcclellan.mj@gmail.com>
2015-10-01 21:34:08 -0500

    Working on code to draw the forecast

--- src/forecast_layer.c
+++ src/forecast_layer.c
@@ -1,11 +1,39 @@
 #include <pebble.h>
 #include "forecast_layer.h"
 
-ForecastLayer* forecast_layer_create_layer(GRect frame, Forecast forecast);
-void forecast_layer_destroy(ForecastLayer *forecast_layer);
-Layer* forecast_layer_get_layer(ForecastLayer *forecast_layer);
-void forecast_layer_set_foreground_color(ForecastLayer *forecast_layer, GColor foreground_color);
-void forecast_layer_set_forecast(ForecastLayer *forecast_layer, Forecast forecast);
+static void draw_forecast(Layer *layer, GContext *ctx) {
+  ForecastLayer *forecast_layer = (ForecastLayer*) layer_get_data(layer);
+  Forecast forecast = forecast_layer->forecast;
+}
+
+ForecastLayer* forecast_layer_create_layer(GRect frame, Forecast forecast) {
+  Layer *layer = layer_create_with_data(frame, sizeof(ForecastLayer));
+  layer_set_hidden(layer, true);
+  layer_set_update_proc(layer, draw_forecast);
+
+  ForecastLayer *forecast_layer = layer_get_data(layer);
+  forecast_layer->forecast = forecast;
+  forecast_layer->background_color = GColorWhite;
+  forecast_layer->foreground_color = GColorBlack;
+
+  return forecast_layer;
+}
+
+void forecast_layer_destroy(ForecastLayer *forecast_layer) {
+  layer_destroy(forecast_layer->layer);
+}
+
+Layer* forecast_layer_get_layer(ForecastLayer *forecast_layer) {
+  return forecast_layer->layer;
+}
+
+void forecast_layer_set_foreground_color(ForecastLayer *forecast_layer, GColor foreground_color) {
+  forecast_layer->foreground_color = foreground_color;
+}
+
+void forecast_layer_set_forecast(ForecastLayer *forecast_layer, Forecast forecast) {
+  forecast_layer->forecast = forecast;
+}
 
 void forecast_layer_set_hidden(ForecastLayer *forecast_layer, bool hidden) {
   layer_set_hidden(forecast_layer->layer, hidden);
--- src/util.c
+++ src/util.c
@@ -9,7 +9,35 @@ uint16_t scale_length_using_int(uint16_t max_size, uint8_t percentage) {
   return scale_length(max_size, as_decimal_percent);
 }
 
-
 GPoint gpoint_add(const GPoint *x, const GPoint *y) {
   return GPoint(x->x + y->x, x->y + y->y);
+}
+
+typedef struct {
+  bool valid;
+  int16_t min;
+  int16_t max;
+} MinMaxResult;
+
+MinMaxResult int16_min_max(int16_t *array, size_t num_elements) {
+  if(num_elements <= 0) {
+    return { .valid = false };
+  }
+
+  MinMaxResult minMaxResult;
+  minMaxResult.min = array[0];
+  minMaxResult.max = array[0];
+
+  for(int i = 0; i < num_elements; i++) {
+    int16_t value = array[i];
+    if(value > minMaxResult.max) {
+      minMaxResult.max = value;
+    }
+
+    if(value < minMaxResult.min) {
+      minMaxResult.min = value;
+    }
+  }
+
+  return minMaxResult;
 }
